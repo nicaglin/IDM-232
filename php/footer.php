e7ac81451c8a01d87d3cb6309ac4201568bf351d
nicaglin <nicaglin@gmail.com>
2017-05-16 06:56:21 -0400

    Update

---
+++ php/footer.php
@@ -0,0 +1,7 @@
+		<footer>
+				<p>&copy; 2017 Veronica G Lin</p>
+		</footer>
+	</div><!-- wrap end -->
+	<script src="../js/main.js"></script>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+	<link rel="stylesheet" href="../css/normalize.css">
+	<link rel="apple-touch-icon" sizes="180x180" href="../img/apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="../img/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="../img/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="../img/manifest.json">
+	<link rel="mask-icon" href="../img/safari-pinned-tab.svg"> <!--color="#5bbad5"-->
+	<meta name="theme-color" content="#ffffff">
+  	<link rel="stylesheet" href="../css/new.css">
+  	<link href="https://fonts.googleapis.com/css?family=Chewy" rel="stylesheet">
+  	<link href="https://fonts.googleapis.com/css?family=Kalam" rel="stylesheet">
+  	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<title>Veronica Lin</title>
+</head>
+<body>
